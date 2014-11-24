065380ca37ba98c8494f7af47af095f1935237b9
Sam Gleske <sam.mxracer@gmail.com>
2014-11-23 23:49:29 -0500

    First working tests with gradle check!

--- src/test/jervis/remotes/GitHubTest.groovy
+++ src/test/jervis/remotes/GitHubTest.groovy
@@ -1,15 +1,48 @@
 package jervis.remotes
+import org.junit.*
 
 class GitHubTest extends GroovyTestCase {
-    protected void setUp() {
+    def mygh
+
+    @Before protected void setUp() {
         super.setUp()
-        def mygh = new GitHub()
+        mygh = new GitHub()
     }
-    def testsetGh_web() {
-        assert mygh.gh_web == "https://github.com/"
+    @After protected void tearDown() {
+        super.tearDown()
+        mygh = null
+    }
+    @Test public void testset1Gh_web() {
         mygh.gh_web = "http://server"
         assert mygh.gh_web == "http://server/"
+    }
+    @Test public void testset2Gh_web() {
         mygh.gh_web = "http://server/"
         assert mygh.gh_web == "http://server/"
+    }
+    @Test public void testreadGh_web() {
+        assert mygh.gh_web == "https://github.com/"
+    }
+    @Test public void testset1Gh_api() {
+        mygh.gh_api = "http://server"
+        assert mygh.gh_api == "http://server/"
+    }
+    @Test public void testset2Gh_api() {
+        mygh.gh_api = "http://server/"
+        assert mygh.gh_api == "http://server/"
+    }
+    @Test public void testreadGh_api() {
+        assert mygh.gh_api == "https://api.github.com/"
+    }
+    @Test public void testset1Gh_clone() {
+        mygh.gh_clone = "http://server"
+        assert mygh.gh_clone == "http://server/"
+    }
+    @Test public void testset2Gh_clone() {
+        mygh.gh_clone = "http://server/"
+        assert mygh.gh_clone == "http://server/"
+    }
+    @Test public void testreadGh_clone() {
+        assert mygh.gh_clone == "git://github.com/"
     }
 }
