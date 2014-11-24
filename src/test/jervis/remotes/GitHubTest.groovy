881c07dbf530f1760cee0a8a2a483791e9f10f7c
Sam Gleske <sam.mxracer@gmail.com>
2014-11-24 00:15:47 -0500

    GitHub tests and renamed tests to include class

--- src/test/jervis/remotes/GitHubTest.groovy
+++ src/test/jervis/remotes/GitHubTest.groovy
@@ -1,48 +1,73 @@
 package jervis.remotes
+//the GitHubTest() class automatically sees the GitHub() class because they're in the same package
 import org.junit.*
 
 class GitHubTest extends GroovyTestCase {
     def mygh
 
+    //set up before every test
     @Before protected void setUp() {
         super.setUp()
         mygh = new GitHub()
     }
+    //tear down after every test
     @After protected void tearDown() {
         super.tearDown()
         mygh = null
     }
-    @Test public void testset1Gh_web() {
+    //test GitHub().gh_web
+    @Test public void test_GitHub_set1Gh_web() {
         mygh.gh_web = "http://server"
         assert mygh.gh_web == "http://server/"
     }
-    @Test public void testset2Gh_web() {
+    @Test public void test_GitHub_set2Gh_web() {
         mygh.gh_web = "http://server/"
         assert mygh.gh_web == "http://server/"
     }
-    @Test public void testreadGh_web() {
+    @Test public void test_GitHub_readGh_web() {
         assert mygh.gh_web == "https://github.com/"
     }
-    @Test public void testset1Gh_api() {
+    //test GitHub().gh_api
+    @Test public void test_GitHub_set1Gh_api() {
         mygh.gh_api = "http://server"
         assert mygh.gh_api == "http://server/"
     }
-    @Test public void testset2Gh_api() {
+    @Test public void test_GitHub_set2Gh_api() {
         mygh.gh_api = "http://server/"
         assert mygh.gh_api == "http://server/"
     }
-    @Test public void testreadGh_api() {
+    @Test public void test_GitHub_readGh_api() {
         assert mygh.gh_api == "https://api.github.com/"
     }
-    @Test public void testset1Gh_clone() {
+    //test GitHub().gh_clone
+    @Test public void test_GitHub_set1Gh_clone() {
         mygh.gh_clone = "http://server"
         assert mygh.gh_clone == "http://server/"
     }
-    @Test public void testset2Gh_clone() {
+    @Test public void test_GitHub_set2Gh_clone() {
         mygh.gh_clone = "http://server/"
         assert mygh.gh_clone == "http://server/"
     }
-    @Test public void testreadGh_clone() {
+    @Test public void test_GitHub_readGh_clone() {
         assert mygh.gh_clone == "git://github.com/"
+    }
+    //test GitHub().gh_token
+    @Test public void test_GitHub_set1Gh_token() {
+        mygh.gh_token = "a"
+        assert mygh.gh_token == "a"
+    }
+    @Test public void test_GitHub_set2Gh_token() {
+        mygh.gh_token = ""
+        assert mygh.gh_token == null
+    }
+    @Test public void test_GitHub_decodeBase64() {
+        def s = "Without tests, you're going to have a bad time!"
+        String encoded = s.bytes.encodeBase64().toString()
+        assert "V2l0aG91dCB0ZXN0cywgeW91J3JlIGdvaW5nIHRvIGhhdmUgYSBiYWQgdGltZSE=" == encoded
+        assert mygh.decodeBase64(encoded) == s
+    }
+    @Test public void test_GitHub_getWebEndpoint() {
+        assert mygh.getWebEndpoint() == mygh.gh_web
+        assert mygh.getWebEndpoint() == "https://github.com/"
     }
 }
