baca4bd529f9931850498addbaacb1310fe9cf0d
Sam Gleske <sam.mxracer@gmail.com>
2014-11-25 03:23:45 -0500

    validator is almost done.

--- src/main/jervis/lang/lifecycleValidator.groovy
+++ src/main/jervis/lang/lifecycleValidator.groovy
@@ -2,13 +2,52 @@ package jervis.lang
 
 import groovy.json.JsonSlurper
 
+//import jervis.lang.lifecycleValidator
+//URL url = new URL('file:///home/sam/git/github/jervis/src/resources/lifecycles.json')
+//def x = new lifecycleValidator()
+//x.load_JSON(url)
+//x.validate()
+
 class lifecycleValidator {
+    def wiki_page = "https://github.com/samrocketman/jervis/wiki/Default-commands-for-supported-languages-file"
     def lifecycles
+    def languages
     def load_JSON(URL url) {
         lifecycles = new groovy.json.JsonSlurper().parse(new File(url.getFile()).newReader())
+        languages = lifecycles.keySet() as String[];
+    }
+    def supportedLanguage(String lang) {
+        lang in languages
+    }
+    def throw_key_exception(rootKey) {
+        throw Exception("\nERROR: Lifecycle validation failed.  Missing key: " + rootKey + ["\n\nSee wiki page:", wiki_page,"\n"].join('\n') )
+    }
+    def throw_value_exception(rootKey, message) {
+        throw Exception("\nERROR: Lifecycle validation failed.  Bad value in key: " + rootKey + message + ["\n\nSee wiki page:", wiki_page,"\n"].join('\n') )
     }
     def validate() {
-        languages = lifecycles.findAll{true}.collect { it.key }
+        lifecycles.keySet().each {
+            println it
+            println "this is a test"
+            println lifecycles[it]["defaultKey"]
+            def tools = lifecycles[it].keySet() as String[]
+            if(!("defaultKey" in tools)) {
+                this.throw_exception([it,"defaultKey"].join('.'))
+            }
+            if(!(lifecycles[it]["defaultKey"] in tools)) {
+                this.throw_exception([it,"defaultKey",lifecycles[it]["defaultKey"]].join('.'))
+            }
+            def current_key = lifecycles[it]["defaultKey"]
+            while(lifecycles[it][current_key] != null) {
+                def cycles = lifecycles[it][current_key].keySet() as String[]
+                if("fileExistsCondition" in cycles) {
+                    //check for leading slash in the first element of fileExistsCondition
+                    if(lifecycles[it][current_key]["fileExistsCondition"][0][0] != '/') {
+                        this.throw_value_exception([it,current_key,"fileExistsCondition","[0]"].join('.'), " first element does not begin with a '/'.")
+                    }
+                }
+            }
+        }
 /*
         lifecycles.findAll{true}.each { language, tools ->
             tools.findAll{true}.each { tool, cycles ->
