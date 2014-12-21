344a7adb9a8a44a71ae750265e0432eea58f2d7b
Sam Gleske <sam.mxracer@gmail.com>
2014-12-21 07:35:07 -0500

    The beginning of a lifecycle generator

---
+++ src/main/jervis/lang/lifecycleGenerator.groovy
@@ -0,0 +1,25 @@
+package jervis.lang
+
+import jervis.lang.lifecycleValidator
+import org.yaml.snakeyaml.Yaml
+
+class lifecycleGenerator {
+    def jervis_yaml
+    def language
+    def lifecycle_obj
+    def lifecycleGenerator() {
+    }
+    def loadLifecycles(URL url) {
+        this.lifecycle_obj = new lifecycleValidator()
+        this.lifecycle_obj.load_JSON(url)
+        this.lifecycle_obj.validate()
+    }
+    def loadYaml(String raw_yaml) {
+        if(!lifecycle_obj) {
+            throw new Exception("bad time")
+        }
+        def yaml = new Yaml()
+        this.jervis_yaml = yaml.load(raw_yaml)
+        this.language = this.jervis_yaml['language']
+    }
+}
