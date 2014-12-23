eadeba78f59d3a2cf038b520c00789342a0e3313
Sam Gleske <sam.mxracer@gmail.com>
2014-12-22 22:41:07 -0500

    groovydoc for MissingKeyException

--- src/main/jervis/exceptions/MissingKeyException.groovy
+++ src/main/jervis/exceptions/MissingKeyException.groovy
@@ -1,8 +1,20 @@
 package jervis.exceptions
 
+/**
+  A type of <code>{@link jervis.exceptions.ValidationException}</code> which is thrown when a lifecycles file key is referenced but missing.
+
+  <h2>Sample usage</h2>
+<pre><code>import jervis.exceptions.MissingKeyException
+throw new MissingKeyException("groovy.defaultKey")</code></pre>
+ */
 
 class MissingKeyException extends ValidationException
 {
+    /**
+      Throw an exception when lifecycles file key is referenced but missing.
+
+      @param message A simple message that will be prepended with <code>"Missing key: " + message</code>
+     */
     def MissingKeyException(String message) {
         super("Missing key: " + message)
     }
