8a4963e3bdd2d2cc46d21a594a20188ab9a37abe
Sam Gleske <sam.mxracer@gmail.com>
2014-12-22 22:40:15 -0500

    groovydoc for lifecycle exceptions

--- src/main/jervis/exceptions/LifecycleException.groovy
+++ src/main/jervis/exceptions/LifecycleException.groovy
@@ -1,6 +1,14 @@
 package jervis.exceptions
 
+/**
+  A group of exceptions that are thrown when undesireable lifecycle generation conditions occur.
+ */
 class LifecycleException extends Exception {
+    /**
+      Throw a <code>LifecycleException</code>
+
+      @param message A simple message.
+     */
     def LifecycleException(String message) {
         super(message)
     }
