47e6138f81fded36e6ad39b437a08e061c4da5d5
Sam Gleske <sam.mxracer@gmail.com>
2014-12-22 22:40:30 -0500

    groovydoc for BadValueInKeyException

--- src/main/jervis/exceptions/BadValueInKeyException.groovy
+++ src/main/jervis/exceptions/BadValueInKeyException.groovy
@@ -1,8 +1,20 @@
 package jervis.exceptions
 
+/**
+  A type of <code>{@link jervis.exceptions.ValidationException}</code> which is thrown when there is a bad value in a lifecycles file key.
+
+  <h2>Sample usage</h2>
+<pre><code>import jervis.exceptions.BadValueInKeyException
+throw new BadValueInKeyException("ruby.rake1.fileExistsCondition")</code></pre>
+ */
 
 class BadValueInKeyException extends ValidationException
 {
+    /**
+      Throw an exception for a bad value in a key.
+
+      @param message A simple message that will be prepended with <code>"Bad value in key: " + message</code>
+     */
     def BadValueInKeyException(String message) {
         super("Bad value in key: " + message)
     }
