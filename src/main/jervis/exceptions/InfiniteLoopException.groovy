73fd477f811d175ad36fe04575faa2e73dbebba6
Sam Gleske <sam.mxracer@gmail.com>
2014-12-22 22:40:50 -0500

    groovydoc for InfiniteLoopException

--- src/main/jervis/exceptions/InfiniteLoopException.groovy
+++ src/main/jervis/exceptions/InfiniteLoopException.groovy
@@ -1,8 +1,20 @@
 package jervis.exceptions
 
+/**
+  A type of <code>{@link jervis.exceptions.ValidationException}</code> which is thrown when a lifecycles file keys reference in an infinite loop.
+
+  <h2>Sample usage</h2>
+<pre><code>import jervis.exceptions.InfiniteLoopException
+throw new InfiniteLoopException("groovy.maven")</code></pre>
+ */
 
 class InfiniteLoopException extends ValidationException
 {
+    /**
+      Throw an exception when lifecycle keys reference in an infinite loop.
+
+      @param message A simple message that will be prepended with <code>"Infinite loop detected.  Last known key: " + message</code>
+     */
     def InfiniteLoopException(String message) {
         super("Infinite loop detected.  Last known key: " + message)
     }
