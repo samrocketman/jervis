3f7e7da096aad9ce8e737cd82519b7d9c68cf206
Sam Gleske <sam.mxracer@gmail.com>
2014-12-22 22:39:58 -0500

    groovydoc for validation exceptions

--- src/main/jervis/exceptions/ValidationException.groovy
+++ src/main/jervis/exceptions/ValidationException.groovy
@@ -2,9 +2,18 @@ package jervis.exceptions
 
 import jervis.exceptions.wikiPages
 
+/**
+  A group of exceptions that are thrown when validation errors occur in the <a href="https://github.com/samrocketman/jervis/wiki/Specification-for-lifecycles-file" target="_blank">lifecycles file</a>.
+ */
+
 class ValidationException extends Exception
 {
     private static String wiki_page = wikiPages.lifecycles_spec
+    /**
+      Throw a <code>ValidationException</code>
+
+      @param message A simple message that will be prepended with <code>"ERROR: Lifecycle validation failed.  " + message</code> as well as provide a link to a helpful wiki page, <code>{@link jervis.exceptions.wikiPages#lifecycles_spec}</code>.
+     */
     def ValidationException(String message) {
         super("\nERROR: Lifecycle validation failed.  " + message + ["\n\nSee wiki page:", wiki_page,"\n"].join('\n'))
     }
