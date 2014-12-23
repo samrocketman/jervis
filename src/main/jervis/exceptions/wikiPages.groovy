ce7b9f1cb4da1d3ea5fe7205ceaa4f9b39254c75
Sam Gleske <sam.mxracer@gmail.com>
2014-12-22 22:41:52 -0500

    groovydoc for wikiPages exception class

--- src/main/jervis/exceptions/wikiPages.groovy
+++ src/main/jervis/exceptions/wikiPages.groovy
@@ -1,6 +1,15 @@
 package jervis.exceptions
 
+/**
+  A simple class, whose only purpose is to house static strings referencing the <a href="https://github.com/samrocketman/jervis/wiki" target="_blank">Jervis wiki</a>, to be used as helpful hints when throwing exceptions.
+ */
 class wikiPages {
+    /**
+      A static reference to the <a href="https://github.com/samrocketman/jervis/wiki/Supported-Languages" target="_blank">supported languages wiki page</a>.
+     */
     public static String supported_languages = "https://github.com/samrocketman/jervis/wiki/Supported-Languages"
+    /**
+      A static reference to the <a href="https://github.com/samrocketman/jervis/wiki/Specification-for-lifecycles-file" target="_blank">lifecycles file spec wiki page</a>.
+     */
     public static String lifecycles_spec = "https://github.com/samrocketman/jervis/wiki/Specification-for-lifecycles-file"
 }
