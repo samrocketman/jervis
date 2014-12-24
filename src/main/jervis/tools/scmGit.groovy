19d41c6dcf5a79361c5f590232e222c07850cf4c
Sam Gleske <sam.mxracer@gmail.com>
2014-12-24 01:37:57 -0500

    groovydoc finished scmGit docs

--- src/main/jervis/tools/scmGit.groovy
+++ src/main/jervis/tools/scmGit.groovy
@@ -10,21 +10,48 @@ def x = new scmGit()
 print "Git root dir: "
 println x.getRoot()</tt></pre><br>
  */
-
 class scmGit {
+    /**
+      Stores the root directory of the git repository when the class is instantiated or <tt>{@link #setRoot()}</tt> is called.
+      Default: <tt>""</tt>
+     */
     String git_root = ""
+
+    /**
+      The default git command to be used when executing git commands.
+      Default: <tt>"git"</tt>
+     */
     String mygit = "git"
+
+    /**
+      Upon instantiation calls the <tt>{@link #setRoot()}</tt> function to set the <tt>{@link #git_root}</tt> property.
+     */
     def scmGit() {
         this.setRoot()
     }
+
+    /**
+      Instantiates a custom <tt>git</tt> path and calls the <tt>{@link #setRoot()}</tt> function to set the <tt>{@link #git_root}</tt> property.
+
+      @param git A path to a <tt>git</tt> executable.
+     */
     def scmGit(String git) {
         mygit = git
         this.setRoot()
     }
+
+    /**
+      Gets the root directory of a repository and stores in in {@link #git_root}.  It is the equivalent of executing the following git command.
+      <pre><tt>git rev-parse --show-toplevel</tt></pre><br>
+     */
     public void setRoot() {
         def process = "${mygit} rev-parse --show-toplevel".execute()
         git_root = process.text.trim()
     }
+
+    /**
+      @return The value of {@link #git_root} which is a known directory of the git repository in the current working directory of when the <tt>scmGit</tt> class was instantiated.
+     */
     String getRoot() {
         git_root
     }
