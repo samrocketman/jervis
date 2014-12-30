90470a0be5ef81915181299dff7f5ba7dcd92183
Sam Gleske <sam.mxracer@gmail.com>
2014-12-29 23:42:01 -0500

    groovydoc for jervis.lang package

---
+++ src/main/jervis/lang/package-info.groovy
@@ -0,0 +1,5 @@
+/**
+  Provides helpers for lifecycles, build tools, and other utilities surrounding supported programming languages.
+  This class should be strictly for classes directly related to supported programming languages.
+ */
+package jervis.lang
