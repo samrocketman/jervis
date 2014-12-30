83abfec9b86835815bcab5bfda06a3903cc9c026
Sam Gleske <sam.mxracer@gmail.com>
2014-12-29 21:46:15 -0500

    groovydoc for jervis.tools package

---
+++ src/main/jervis/tools/package-info.groovy
@@ -0,0 +1,6 @@
+/**
+  Contains miscellaneous security tools and other doodads.  The tools contained in this
+  package are meant to enhance the capabilities of Jervis by providing classes which
+  don't make sense as single functions in the Job DSL scripts.
+ */
+package jervis.tools
