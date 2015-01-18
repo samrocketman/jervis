6c47c8e3a990f3fe1cb6a5f9aca59164c8cdd9e8
Sam Gleske <sam.mxracer@gmail.com>
2015-01-18 16:00:49 -0500

    Adding supported tools wiki page to wikiPages

--- src/main/jervis/exceptions/wikiPages.groovy
+++ src/main/jervis/exceptions/wikiPages.groovy
@@ -12,6 +12,11 @@ class wikiPages {
     public static String supported_languages = "https://github.com/samrocketman/jervis/wiki/Supported-Languages"
 
     /**
+      A static reference to the <a href="https://github.com/samrocketman/jervis/wiki/Supported-Tools" target="_blank">supported tools wiki page</a>.
+     */
+    public static String supported_tools = "https://github.com/samrocketman/jervis/wiki/Supported-Tools"
+
+    /**
       A static reference to the <a href="https://github.com/samrocketman/jervis/wiki/Specification-for-lifecycles-file" target="_blank">lifecycles file spec wiki page</a>.
      */
     public static String lifecycles_spec = "https://github.com/samrocketman/jervis/wiki/Specification-for-lifecycles-file"
