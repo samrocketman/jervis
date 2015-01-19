e868d99faa82a8b7d001f53325b2bc99304e5852
Sam Gleske <sam.mxracer@gmail.com>
2015-01-18 22:18:05 -0500

    lifecycleGenerator: Test generateToolchainSection

--- src/test/jervis/lang/lifecycleGeneratorTest.groovy
+++ src/test/jervis/lang/lifecycleGeneratorTest.groovy
@@ -1,7 +1,8 @@
 package jervis.lang
 //the lifecycleGeneratorTest() class automatically sees the lifecycleGenerator() class because they're in the same package
-import org.junit.*
 import jervis.exceptions.UnsupportedLanguageException
+import jervis.exceptions.UnsupportedToolException
+import org.junit.*
 
 class lifecycleGeneratorTest extends GroovyTestCase {
     def generator
@@ -33,5 +34,27 @@ class lifecycleGeneratorTest extends GroovyTestCase {
     @Test public void test_lifecycleGenerator_isMatrixBuild_true() {
         generator.loadYaml("language: ruby\nenv:\n  - foobar=foo\n  - foobar=bar")
         assert true == generator.isMatrixBuild()
+    }
+    @Test public void test_lifecycleGenerator_generateToolchainSection_nonmatrix() {
+        URL url = this.getClass().getResource("/good_lifecycles_simple.json");
+        generator.loadLifecycles(url.getFile())
+        url = this.getClass().getResource("/good_toolchains_simple.json");
+        generator.loadToolchains(url.getFile())
+        generator.loadYaml("language: ruby")
+        assert '#\n# TOOLCHAINS SECTION\n#\n#gemfile toolchain section\nexport BUNDLE_GEMFILE="${PWD}/Gemfile"\n#env toolchain section\n#rvm toolchain section\nsome commands\n#jdk toolchain section\nsome commands\n' == generator.generateToolchainSection()
+        generator.loadYaml("language: ruby\nenv: foo=bar")
+        assert '#\n# TOOLCHAINS SECTION\n#\n#gemfile toolchain section\nexport BUNDLE_GEMFILE="${PWD}/Gemfile"\n#env toolchain section\nexport foo=bar\n#rvm toolchain section\nsome commands\n#jdk toolchain section\nsome commands\n' == generator.generateToolchainSection()
+        generator.loadYaml("language: ruby\nenv:\n  - foo=bar")
+        assert '#\n# TOOLCHAINS SECTION\n#\n#gemfile toolchain section\nexport BUNDLE_GEMFILE="${PWD}/Gemfile"\n#env toolchain section\nexport foo=bar\n#rvm toolchain section\nsome commands\n#jdk toolchain section\nsome commands\n' == generator.generateToolchainSection()
+        generator.loadYaml("language: ruby\njdk: openjdk7")
+        assert '#\n# TOOLCHAINS SECTION\n#\n#gemfile toolchain section\nexport BUNDLE_GEMFILE="${PWD}/Gemfile"\n#env toolchain section\n#rvm toolchain section\nsome commands\n#jdk toolchain section\nsome commands\n' == generator.generateToolchainSection()
+        generator.loadYaml("language: ruby\njdk: derp")
+        shouldFail(UnsupportedToolException) {
+            generator.generateToolchainSection()
+        }
+        generator.loadYaml("language: ruby\njdk: 2.5")
+        shouldFail(UnsupportedToolException) {
+            generator.generateToolchainSection()
+        }
     }
 }
