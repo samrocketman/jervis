35238401f9f23883f095b228b472ad57ebb73b88
Sam Gleske <sam.mxracer@gmail.com>
2015-01-26 20:10:34 -0500

    Update groovydoc for jervis.remotes package.

--- src/main/jervis/remotes/package-info.groovy
+++ src/main/jervis/remotes/package-info.groovy
@@ -1,27 +1,8 @@
 /**
-  Contains <tt>remotes</tt> for Jervis to communicate to different sources hosting Git repositories.
-  If writing a remote, then it is required to have at a minimum the following functions.
+  Contains <tt>remotes</tt> for Jervis to communicate to different sources hosting
+  Git repositories.  If writing a remote, be sure to follow the implementation of
+  the <tt>JervisRemote</tt> interface.
 
-  <ul>
-      <li>
-          <tt>String getWebUrl()</tt> returns a base web URL in which to browse code.
-      </li>
-      <li>
-          <tt>String getCloneUrl()</tt> returns a base git URI in which a repository can be cloned.
-      </li>
-      <li>
-          <tt>ArrayList branches(String project)</tt> returns a <tt>List</tt> where each element is a branch in the project.
-      </li>
-      <li>
-          <tt>String getFile(String project, String file_path, String ref)</tt> returns a <tt>String</tt> which contains the contents of the file requested.
-      </li>
-      <li>
-          <tt>ArrayList getFolderListing(String project, String dir_path, String ref)</tt> returns an <tt>ArrayList</tt> which contains the list of files in the requested path.
-      </li>
-      <li>
-          <tt>String toString()</tt> returns a human readable <tt>String</tt> for this type of remote.
-      </li>
-  </ul>
   <p>
   This allows remotes to be interchangeable.  Jervis will call only those functions to interact with a remote.
   See the <tt>GitHub</tt> class as an example for the implementation of a remote.
