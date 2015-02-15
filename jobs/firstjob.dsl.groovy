79f6a1a1e856dc3044755eabdc8cd48d6db40585
Sam Gleske <sam.mxracer@gmail.com>
2015-02-15 13:17:59 -0500

    firstjob need to set job type

--- jobs/firstjob.dsl.groovy
+++ jobs/firstjob.dsl.groovy
@@ -56,7 +56,14 @@ if("${project}".size() > 0 && "${project}".split('/').length == 2) {
             return
         }
         generator.folder_listing = folder_listing
-        job {
+        def jobType
+        if(generator.isMatrixBuild()) {
+            jobType = MatrixJob
+        }
+        else {
+            jobType = Freeform
+        }
+        job(type: jobType) {
             name("${project_folder}/" + "${project_name}-${JERVIS_BRANCH}".replaceAll('/','-'))
             scm {
                 //see https://github.com/jenkinsci/job-dsl-plugin/pull/108
