ba2b2dbb722932be59f91c009832fe2785a78c83
Sam Gleske <sam.mxracer@gmail.com>
2015-02-15 19:25:26 -0500

    Fix exit status of assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set +e
 count=0
+STATUS=0
 while ! ./gradlew assemble;do
+  STATUS=$?
   if [ "${count}" = 3 ];then
     break
   fi
+  echo "Failed to assemble dependencies.  Sleep 5 and retry."
   sleep 5
   ((count++))
 done
+exit $STATUS
