ae05b8798cc4228f610d24d543557cfb4e577920
Sam Gleske <sam.mxracer@gmail.com>
2015-02-21 15:29:04 -0500

    Bugfix fileIO groovydoc and 80 chars wide

--- src/main/groovy/jervis/tools/fileIO.groovy
+++ src/main/groovy/jervis/tools/fileIO.groovy
@@ -2,9 +2,13 @@ package jervis.tools
 import jervis.tools.scmGit
 
 /**
-  A class to provide basic file operations from <tt>readFile()</tt> strings in a yaml file.
-  <tt>readFile()</tt> strings typically have the format of <tt>readFile(/path/to/file.sh)</tt> where <tt>/path/to/file.sh</tt> is a bash script that exists relative to the root of the Git repository.
-  This is intended to simplify a yaml file by keeping complicated bash code in a file and allowing the <tt>readFile()</tt> command to read it as if it were written in the yaml file.
+  A class to provide basic file operations from <tt>readFile()</tt> strings in a
+  yaml file.  <tt>readFile()</tt> strings typically have the format of
+  <tt>readFile(/path/to/file.sh)</tt> where <tt>/path/to/file.sh</tt> is a bash
+  script that exists relative to the root of the Git repository.  This is intended
+  to simplify a yaml file by keeping complicated bash code in a file and allowing
+  the <tt>readFile()</tt> command to read it as if it were written in the yaml
+  file.
 
   <h2>Sample usage</h2>
 <pre><tt>import jervis.tools.fileIO
@@ -18,9 +22,11 @@ println x.readFile(mystring)</tt></pre>
  */
 class fileIO {
     /**
-      Checks if the <tt>String</tt> starts with <tt>readFile(</tt> and ends with <tt>)</tt>.
+      Checks if the <tt>String</tt> starts with <tt>readFile(</tt> and ends with
+      <tt>)</tt>.
       @param filestring A simple <tt>String</tt>.
-      @return           A boolean based on the outcome of the description of this section.
+      @return           A boolean based on the outcome of the description of this
+                        section.
      */
     public Boolean isReadFile(String filestring) {
         return (filestring =~ /^readFile\(\/.*\)$/).asBoolean()
@@ -29,7 +35,7 @@ class fileIO {
       Checks the file path inside the parenthesis of
       <tt>'readFile(/path/to/file.sh)'</tt> exists.  Relative to the root of the Git
       repository, if the file <tt>/path/to/file.sh</tt> exists then it would return
-      <tt>true</tt>.  This method uses <tt>{@link jervis.tools.scmGit</tt> to
+      <tt>true</tt>.  This method uses <tt>{@link jervis.tools.scmGit}</tt> to
       determine the root of the Git repository.
       @param filestring A simple <tt>String</tt> that starts with <tt>readFile(</tt>, contains a path to a file, and ends with <tt>)</tt>.
       @return           A boolean.  If the file exists then it is <tt>true</tt>.  If the the file doesn't exist or it is a malformed <tt>String</tt> then it returns <tt>false</tt>.
@@ -46,8 +52,8 @@ class fileIO {
     }
     /**
       Read the contents of the file in a <tt>readFile()</tt> <tt>String</tt>.  This
-      method uses <tt>{@link jervis.tools.scmGit</tt> to determine the root of the Git
-      repository.
+      method uses <tt>{@link jervis.tools.scmGit}</tt> to determine the root of the
+      Git repository.
       @param filestring Read the contents of <tt>/path/to/file.sh</tt> in the <tt>String</tt> <tt>readFile(/path/to/file.sh)</tt>.
       @return           A <tt>String</tt> which is the contents of the file read.  If the <tt>filestring</tt> is malformed or the file doesn't exist then it simply returns the original string.
      */
