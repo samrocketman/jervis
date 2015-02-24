93348b757a97ed99b72e7f1275766a5edecd7907
Sam Gleske <sam.mxracer@gmail.com>
2015-02-23 21:39:31 -0500

    License change from GPLv2 to Apache 2.0

--- src/test/groovy/jervis/tools/scmGitTest.groovy
+++ src/test/groovy/jervis/tools/scmGitTest.groovy
@@ -1,3 +1,18 @@
+/*
+   Copyright 2014-2015 Sam Gleske - https://github.com/samrocketman/jervis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
 package jervis.tools
 //the scmGitTest() class automatically sees the scmGit() class because they're in the same package
 import jervis.exceptions.JervisException
