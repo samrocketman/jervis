4f88e6e44bfb8413ff40a213be619d30780be2cc
Sam Gleske <sam.mxracer@gmail.com>
2015-07-18 23:28:32 -0400

    Some platformValidator tests

---
+++ src/test/groovy/jervis/lang/platformValidatorTest.groovy
@@ -0,0 +1,64 @@
+/*
+   Copyright 2014-2015 Sam Gleske - https://github.com/samrocketman/jervis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+package jervis.lang
+//the platformValidatorTest() class automatically sees the platformValidator() class because they're in the same package
+import jervis.exceptions.PlatformBadValueInKeyException
+import jervis.exceptions.PlatformMissingKeyException
+import jervis.exceptions.PlatformValidationException
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class platformValidatorTest extends GroovyTestCase {
+    def platforms
+    //set up before every test
+    @Before protected void setUp() {
+        super.setUp()
+        platforms = new platformValidator()
+    }
+    //tear down after every test
+    @After protected void tearDown() {
+        platforms = null
+        super.tearDown()
+    }
+    @Test public void test_platformValidator_load_JSON() {
+        assert platforms.platforms == null
+        URL url = this.getClass().getResource('/good_platforms_simple.json')
+        platforms.load_JSON(url.getFile())
+        assert platforms.platforms instanceof Map
+        assert platforms.platforms['defaults']['platform'] == 'docker'
+    }
+    @Test public void test_platformValidator_load_JSONString() {
+        assert platforms.platforms == null
+        URL url = this.getClass().getResource('/good_platforms_simple.json')
+        String contents = new File(url.getFile()).getText()
+        //use a string this time
+        platforms.load_JSONString(contents)
+        assert platforms.platforms instanceof Map
+        assert platforms.platforms['defaults']['platform'] == 'docker'
+    }
+    @Test public void test_platformValidator_good_platforms_simple() {
+        URL url = this.getClass().getResource('/good_platforms_simple.json');
+        platforms.load_JSON(url.getFile())
+        assert true == platforms.validate()
+        assert true == platforms.validate_asBool()
+    }
+    @Test public void test_platformValidator_main_platforms_json() {
+        URL url = this.getClass().getResource('/platforms.json');
+        platforms.load_JSON(url.getFile())
+        assert true == platforms.validate()
+    }
+}
