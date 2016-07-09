e2fa6e05af7d00fd313efd44b408fb73f7fbc7e7
Sam Gleske <sam.mxracer@gmail.com>
2016-07-09 16:16:45 -0700

    clean up on exit trap

--- assets/footer.sh
+++ assets/footer.sh
@@ -1,11 +1,1 @@
 #denotes the end of a section
-set +x
-
-if [ "${JERVIS_LANG}" = 'ruby' ]; then
-  rvm gemset delete "${JERVIS_GEMSET}" --force
-fi
-
-if [ "${JERVIS_LANG}" = 'go' ]; then
-  cd -
-  rm -rf "${GOPATH}"
-fi
--- assets/header.sh
+++ assets/header.sh
@@ -11,4 +11,20 @@ echo -n "Bash: "
 bash --version | head -n1
 export JERVIS_RANDOM="${RANDOM}"
 echo "JERVIS_RANDOM: ${JERVIS_RANDOM}"
+
+#clean up on exit
+function cleanup_on() {
+  set +x
+
+  if [ "${JERVIS_LANG}" = 'ruby' ]; then
+    rvm gemset delete "${JERVIS_GEMSET}" --force
+  fi
+
+  if [ "${JERVIS_LANG}" = 'go' ]; then
+    cd -
+    rm -rf "${GOPATH}"
+  fi
+}
+trap cleanup_on EXIT
+
 set -axeE
