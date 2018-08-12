6f555475749229e1f2d99758d15f005939f02984
Sam Gleske <sam.mxracer@gmail.com>
2018-08-12 15:17:45 -0700

    Pipeline: factor out publish results stage

--- vars/buildViaJervis.groovy
+++ vars/buildViaJervis.groovy
@@ -21,54 +21,7 @@ import net.gleske.jervis.lang.lifecycleGenerator
 import net.gleske.jervis.lang.pipelineGenerator
 
 /**
-  Process default publishable items provided by this script.
- */
-def processDefaultPublishable(def item, String publishable, boolean is_pull_request) {
-    switch(publishable) {
-        case 'artifacts':
-            archiveArtifacts artifacts: item['path'], fingerprint: true,
-                             excludes: item['excludes'],
-                             allowEmptyArchive: item['allowEmptyArchive'],
-                             defaultExcludes: item['defaultExcludes'],
-                             caseSensitive: item['caseSensitive']
-            break
-        case 'cobertura':
-            cobertura coberturaReportFile: item['path'],
-                      autoUpdateHealth: item['autoUpdateHealth'],
-                      autoUpdateStability: item['autoUpdateStability'],
-                      failNoReports: item['failNoReports'],
-                      failUnhealthy: item['failUnhealthy'],
-                      failUnstable: item['failUnstable'],
-                      maxNumberOfBuilds: item['maxNumberOfBuilds'],
-                      onlyStable: item['onlyStable'],
-                      sourceEncoding: item['sourceEncoding'],
-                      zoomCoverageChart: item['zoomCoverageChart'],
-                      methodCoverageTargets: item['methodCoverageTargets'],
-                      lineCoverageTargets: item['lineCoverageTargets'],
-                      conditionalCoverageTargets: item['conditionalCoverageTargets']
-            break
-        case 'html':
-            publishHTML allowMissing: item['allowMissing'],
-                        alwaysLinkToLastBuild: item['alwaysLinkToLastBuild'],
-                        includes: item['includes'],
-                        keepAll: item['keepAll'],
-                        reportDir: item['path'],
-                        reportFiles: item['reportFiles'],
-                        reportName: item['reportName'],
-                        reportTitles: item['reportTitles']
-            break
-        case 'junit':
-            junit allowEmptyResults: item['allowEmptyResults'],
-                  healthScaleFactor: item['healthScaleFactor'],
-                  keepLongStdio: item['keepLongStdio'],
-                  testResults: item['path']
-            break
-    }
-}
-
-
-/**
-  The main method of buildViaJervis()
+  call() is the main method of buildViaJervis()
  */
 def call() {
     def global_scm = scm
@@ -119,26 +72,8 @@ def call() {
         if(!generator.isMatrixBuild()) {
             buildProjectStage(global_scm, generator, pipeline_generator, jervisEnvList, script_header, script_footer)
         }
-        List publishableItems = pipeline_generator.publishableItems
-        if(publishableItems) {
-            stage("Publish results") {
-                //unstash and publish in parallel
-                Map tasks = [failFast: true]
-                for(String publishable : publishableItems) {
-                    String publish = publishable
-                    tasks["Publish ${publish}"] = {
-                        try {
-                            unstash publish
-                            processDefaultPublishable(pipeline_generator.getPublishable(publish), publish, is_pull_request)
-                        }
-                        catch(e) {
-                            currentBuild.result = 'FAILURE'
-                        }
-                    }
-                }
-                parallel(tasks)
-            }
-        }
+        publishResultsStage(generator, pipeline_generator)
+
         if(currentBuild.result == 'FAILURE') {
             error 'This build has failed.  No user-defined pipelines will be run.'
         }
---
+++ vars/publishResultsStage.groovy
@@ -0,0 +1,94 @@
+/*
+   Copyright 2014-2018 Sam Gleske - https://github.com/samrocketman/jervis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+/*
+   If a user has defined collecting results in their YAML then this stage will
+   publish them.
+ */
+
+import net.gleske.jervis.lang.lifecycleGenerator
+import net.gleske.jervis.lang.pipelineGenerator
+
+/**
+  Process default publishable items provided by this script.
+ */
+def processDefaultPublishable(def item, String publishable, boolean is_pull_request) {
+    switch(publishable) {
+        case 'artifacts':
+            archiveArtifacts artifacts: item['path'], fingerprint: true,
+                             excludes: item['excludes'],
+                             allowEmptyArchive: item['allowEmptyArchive'],
+                             defaultExcludes: item['defaultExcludes'],
+                             caseSensitive: item['caseSensitive']
+            break
+        case 'cobertura':
+            cobertura coberturaReportFile: item['path'],
+                      autoUpdateHealth: item['autoUpdateHealth'],
+                      autoUpdateStability: item['autoUpdateStability'],
+                      failNoReports: item['failNoReports'],
+                      failUnhealthy: item['failUnhealthy'],
+                      failUnstable: item['failUnstable'],
+                      maxNumberOfBuilds: item['maxNumberOfBuilds'],
+                      onlyStable: item['onlyStable'],
+                      sourceEncoding: item['sourceEncoding'],
+                      zoomCoverageChart: item['zoomCoverageChart'],
+                      methodCoverageTargets: item['methodCoverageTargets'],
+                      lineCoverageTargets: item['lineCoverageTargets'],
+                      conditionalCoverageTargets: item['conditionalCoverageTargets']
+            break
+        case 'html':
+            publishHTML allowMissing: item['allowMissing'],
+                        alwaysLinkToLastBuild: item['alwaysLinkToLastBuild'],
+                        includes: item['includes'],
+                        keepAll: item['keepAll'],
+                        reportDir: item['path'],
+                        reportFiles: item['reportFiles'],
+                        reportName: item['reportName'],
+                        reportTitles: item['reportTitles']
+            break
+        case 'junit':
+            junit allowEmptyResults: item['allowEmptyResults'],
+                  healthScaleFactor: item['healthScaleFactor'],
+                  keepLongStdio: item['keepLongStdio'],
+                  testResults: item['path']
+            break
+    }
+}
+
+def call(lifecycleGenerator generator, pipelineGenerator pipeline_generator) {
+    List publishableItems = pipeline_generator.publishableItems
+    if(publishableItems) {
+        stage("Publish results") {
+            //unstash and publish in parallel
+            Map tasks = [failFast: true]
+            for(String publishable : publishableItems) {
+                // This "publish" variable is required because of how CPS
+                // strangely behaves with tasks and looping.  Without it,
+                // pipeline makes publishable null in tasks oddly.
+                String publish = publishable
+                tasks["Publish ${publish}"] = {
+                    try {
+                        unstash publish
+                        processDefaultPublishable(pipeline_generator.getPublishable(publish), publish, is_pull_request)
+                    }
+                    catch(e) {
+                        currentBuild.result = 'FAILURE'
+                    }
+                }
+            }
+            parallel(tasks)
+        }
+    }
+}
