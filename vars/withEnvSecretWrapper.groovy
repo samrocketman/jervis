40146db801d0fc607ebe509df7282dd7cdb0277d
Sam Gleske <sam.mxracer@gmail.com>
2018-08-12 13:41:13 -0700

    Pipeline: Factor out withEnvSecretWrapper

---
+++ vars/withEnvSecretWrapper.groovy
@@ -0,0 +1,39 @@
+/*
+   Copyright 2014-2018 Sam Gleske - https://github.com/samrocketman/jervis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+/**
+  An environment wrapper which sets environment variables.  If available, also
+  sets and masks decrypted properties from .jervis.yml.
+ */
+
+import net.gleske.jervis.lang.pipelineGenerator
+
+def call(pipelineGenerator generator, List envList, Closure body) {
+    List spe = generator.secretPairsEnv
+    List secretPairs = spe[0]
+    List secretEnv = spe[1]
+    if(secretPairs) {
+        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: secretPairs]) {
+            withEnv(secretEnv + envList) {
+                body()
+            }
+        }
+    }
+    else {
+        withEnv(envList) {
+            body()
+        }
+    }
+}
