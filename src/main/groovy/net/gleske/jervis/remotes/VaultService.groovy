3a554c938771dcb866a92c580d219a8cb9406283
Sam Gleske <sam.mxracer@gmail.com>
2020-08-18 18:56:49 -0400

    Recursive key traversing
    - Enables me to find all keys and subkeys if given a vault path.
    - Moving away from "V2" methods to just generic getSecret method (and
      other similar "V2" methods).  Now, both Key-Value engine 1 and 2 are
      supported in the same method.
    - For extended mount point permission this library can read the mount
      tune to detect the Key-Value engine version.  But, manually setting the
      Key-Value engine version per mount point is also supported.
    - Lots of basic Java Doc added.  I plan to add a lot  more extended Java
      Doc which will eventually include several examples on how to use this
      class.
    - Some scaffold for additional methods I want to support have been added.
      Implementation is TODO.

--- src/main/groovy/net/gleske/jervis/remotes/VaultService.groovy
+++ src/main/groovy/net/gleske/jervis/remotes/VaultService.groovy
@@ -1,13 +1,18 @@
 package net.gleske.jervis.remotes
 
 import net.gleske.jervis.remotes.interfaces.TokenCredential
+import net.gleske.jervis.exceptions.JervisException
 
 class VaultService implements SimpleRestServiceSupport {
     private final String vault_url
     private final TokenCredential credential
+    Map mountVersions = [:]
 
     VaultService(String vault_url, TokenCredential credential) {
         this.vault_url = (vault_url[-1] == '/')? vault_url : vault_url + '/'
+        if(!this.vault_url.endsWith('v1/')) {
+            this.vault_url += 'v1/'
+        }
         this.credential = credential
     }
 
@@ -20,16 +25,122 @@ class VaultService implements SimpleRestServiceSupport {
         headers
     }
 
-    Map getSecretV2(String path, int version = 0) {
-        String engine = path -~ '/.*$'
+    /*
+       Get secret from a KV v1 or v2 secret engine.
+      */
+    Map getSecret(String path, int version = 0) {
+        String mount = path -~ '/.*$'
         String subpath = path -~ '^[^/]+/'
-        apiFetch("v1/${engine}/data/${subpath}?version=${version}")?.data?.data
+        if(isKeyValueV2(mount)) {
+            apiFetch("${mount}/data/${subpath}?version=${version}")?.data?.data
+        }
+        else {
+            apiFetch(path)
+        }
     }
 
-    List listSecretV2(String path) {
-        String engine = path -~ '/.*$'
+    void setSecret(String path, Map secret) {
+        String mount = path -~ '/.*$'
+        String subpath = path -~ '^[^/]+/'
+        if(isKeyValueV2(mount)) {
+            apiFetch("${mount}/data/${subpath}?version=${version}")?.data?.data
+        }
+        else {
+            apiFetch(path, [:], 'POST', objToJson(secret))
+        }
+    }
+
+    void setMountVersions(String mount, def version) {
+        if(!(version in ['1', '2'])) {
+            throw new JervisException('Error: Vault key-value mounts can only be version "1" or "2" (String).')
+        }
+        this.mountVersions[mount] = version
+    }
+
+    void setMountVersions(Map mountVersions) {
+        mountVersions.each { k, v ->
+            this.setMountVersions(k, v)
+        }
+    }
+
+    /**
+      Checks version of a Key-Value engine mount.  Returns true if Key-Value v2
+      or false if Key-Value v2.
+      */
+    private Boolean isKeyValueV2(String mount) {
+        discoverMountVersion(mount)
+        this.mountVersions[mount] == '2'
+    }
+
+    /**
+      If the mount is not already in mountVersions, then this will inspect the
+      mount and set whether the mount is a Key-Value secret engine v1 or v2.
+      */
+    private void discoverMountVersion(String mount) {
+        if(mount in this.mountVersions) {
+            return
+        }
+        setMountVersions(mount, apiFetch("sys/mounts/${mount}/tune")?.options?.version)
+    }
+
+    List listPath(String path) {
+        String mount = path -~ '/.*$'
         String subpath = path -~ '^[^/]+/'
         subpath = (!subpath || subpath[-1] == '/')? subpath : subpath + '/'
-        apiFetch("v1/${engine}/metadata/${subpath}?list=true")?.data?.keys
+
+        if(isKeyValueV2(mount)) {
+            apiFetch("${mount}/metadata/${subpath}?list=true")?.data?.keys
+        }
+        else {
+            // KV v1 API call here
+            apiFetch("${mount}/${subpath}?list=true")?.data?.keys
+        }
+    }
+
+    /**
+      Internal method used by findAllKeys() which tracks recursion level in addition to the user-passed settings.
+      */
+    private List recursiveFindAllKeys(String path, Integer desiredLevel, Integer level) {
+        if(desiredLevel > 0 && level > desiredLevel) {
+            return []
+        }
+        List entries = listPath(path).collect { String key ->
+            if(key.endsWith('/')) {
+                recursiveFindAllKeys(path + key, desiredLevel, level + 1)
+            }
+            else {
+                [path + key]
+            }
+        }
+
+        if(entries) {
+            entries.sum()
+        }
+        else {
+            []
+        }
+    }
+
+    /**
+      Recursively traverses the path for subkeys.  If level is 0 then there's
+      no depth limit.  When level = n, keys are traversed up to the limit.
+       */
+    List findAllKeys(String path, Integer level = 0) {
+        recursiveFindAllKeys(path, level, 1)
+    }
+
+    /**
+      Copies the contents of secret from a source key, srcKey, to the
+      destination key, destKey.
+      */
+    void copySecret(String srcKey, String destKey) {
+        setSecret(destKey, getSecret(srcKey))
+    }
+
+    /**
+      Recursively copies all secrets from the source path, srcPath, to the
+      destination path, destPath.
+      */
+    void copyAllKeys(String srcPath, destPath, Integer level = 0) {
     }
 }
