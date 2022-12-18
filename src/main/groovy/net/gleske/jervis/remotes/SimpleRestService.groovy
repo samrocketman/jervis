c2c3d11b734b0558300aec009e930bd1d4a46c25
Sam Gleske <sam.mxracer@gmail.com>
2022-12-18 00:33:34 -0500

    Reorganize comment for clarity

--- src/main/groovy/net/gleske/jervis/remotes/SimpleRestService.groovy
+++ src/main/groovy/net/gleske/jervis/remotes/SimpleRestService.groovy
@@ -94,9 +94,9 @@ class SimpleRestService {
             // Error checking the verb is not necessary.  For example, HashiCorp
             // Vault has custom HTTP verbs such as LIST.
             // source: https://github.com/AdoptOpenJDK/openjdk-jdk11/blob/master/src/java.base/share/classes/java/net/HttpURLConnection.java
+            conn.@method = http_method.toUpperCase()
             // Necessary for mock interception
             conn.setRequestProperty('X-HTTP-Method-Override', http_method)
-            conn.@method = http_method.toUpperCase()
             conn.setRequestProperty('X-HTTP-Method-Override', null)
 
             if(conn.getDoOutput()) {
