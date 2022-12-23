9214e2db05d2a5b61e72b1e16c33a29de9a16b5c
Sam Gleske <sam.mxracer@gmail.com>
2022-12-22 23:55:05 -0500

    Update mock data generation script

--- src/test/groovy/net/gleske/jervis/remotes/VaultServiceTest.groovy
+++ src/test/groovy/net/gleske/jervis/remotes/VaultServiceTest.groovy
@@ -56,7 +56,7 @@ if(binding.hasVariable('request_history')) {
 import net.gleske.jervis.remotes.interfaces.TokenCredential
 import net.gleske.jervis.remotes.VaultService
 
-TokenCredential creds = [getToken: {-> 'hvs.CT1912OdOBRWnn1UVQntX9Ld' }] as TokenCredential
+TokenCredential creds = [getToken: {-> 'hvs.bu4PfApCPrpSL0P1iOfC8EDE' }] as TokenCredential
 VaultService myvault = new VaultService('http://vault:8200/v1/', creds)
 
 myvault.discoverKVMounts()
@@ -65,11 +65,12 @@ myvault.discoverKVMounts()
 Boolean init_kv = false
 Boolean init_kv_cas = false
 Boolean init_secret = false
+Boolean init_kv_slash = false
+Boolean init_secret_slash = false
 if(!('kv' in myvault.mountVersions.keySet())) {
     myvault.apiFetch('sys/mounts/kv', [:], 'POST', '{"type": "kv", "options": {"version": "2"}}')
     init_kv = true
 }
-
 if(!('kv_cas' in myvault.mountVersions.keySet())) {
     myvault.apiFetch('sys/mounts/kv_cas', [:], 'POST', '{"type": "kv", "options": {"version": "2"}}')
     myvault.apiFetch('kv_cas/config', [:], 'POST', '{"cas_required":true}')
@@ -78,6 +79,14 @@ if(!('kv_cas' in myvault.mountVersions.keySet())) {
 if(!('secret' in myvault.mountVersions.keySet())) {
     myvault.apiFetch('sys/mounts/secret', [:], 'POST', '{"type": "kv", "options": {"version": "1"}}')
     init_secret = true
+}
+if(!('kv2/withslash' in myvault.mountVersions.keySet())) {
+    myvault.apiFetch('sys/mounts/kv2/withslash', [:], 'POST', '{"type": "kv", "options": {"version": "2"}}')
+    init_kv_slash = true
+}
+if(!('secret2/withslash' in myvault.mountVersions.keySet())) {
+    myvault.apiFetch('sys/mounts/secret2/withslash', [:], 'POST', '{"type": "kv", "options": {"version": "1"}}')
+    init_secret_slash = true
 }
 
 // Discover mounts after potentially adding new secrets engines.
@@ -162,7 +171,80 @@ myvault.setSecret('kv/v2_force_cas', [test: 'data'], true)
 myvault.setSecret('kv/v2_force_cas_update', [test: 'update'], true)
 myvault.setSecret('kv_cas/v2_detect_cas', [another: 'secret', hello: 'world'])
 myvault.setSecret('kv_cas/data_to_update', [update: 'secret'])
-println 'Success.'</tt></pre>
+
+// Test recursive copy and delete operations
+myvault.copyAllKeys('kv/foo', 'kv2/withslash/rename')
+myvault.copyAllKeys('kv2/withslash/rename', 'secret2/withslash/subpath/')
+myvault.copyAllKeys('secret/foo', 'secret2/withslash/rename')
+myvault.copyAllKeys('secret2/withslash/rename', 'kv2/withslash/subpath/')
+myvault.isDeletedKey('kv/foo')
+myvault.isDeletedKey('secret/foo')
+myvault.isDeletedKey('secret/foo2')
+myvault.isDeletedKey('kv/foo2')
+myvault.copySecret('secret/foo', 'kv2/withslash/deleteone')
+myvault.copySecret('kv/foo', 'kv2/withslash/deleteone', 1)
+myvault.copySecret('secret/foo', 'kv2/withslash/deleteone')
+myvault.copySecret('kv/foo', 'kv2/withslash/deleteone')
+myvault.deleteKey( 'kv2/withslash/deleteone')
+myvault.deleteKey( 'kv2/withslash/deleteone', [1])
+myvault.deleteKey( 'kv2/withslash/deleteone', [3], true)
+myvault.isDeletedKey('kv2/withslash/deleteone')
+myvault.isDeletedKey('kv2/withslash/deleteone', 1)
+myvault.isDeletedKey('kv2/withslash/deleteone', 2)
+myvault.isDeletedKey('kv2/withslash/deleteone', 3)
+myvault.isDeletedKey('kv2/withslash/deleteone', 4)
+myvault.getSecret('kv2/withslash/deleteone', 2)
+myvault.copyAllKeys('kv/foo', 'kv2/withslash/somepath')
+myvault.copyAllKeys('kv/foo', 'secret2/withslash/somepath')
+myvault.deletePath('kv2/withslash/somepath')
+myvault.deletePath('kv2/withslash/somepath', true)
+myvault.deletePath('secret2/withslash/somepath')
+
+// Rediscover mounts
+myvault.discoverKVMounts()
+
+// Read operations
+myvault.getSecret('secret/foo')
+myvault.getSecret('secret/foo/bar')
+myvault.getSecret('secret/foo/bar/baz')
+myvault.getSecret('kv/foo')
+myvault.getSecret('kv/foo/bar')
+myvault.getSecret('kv/foo/bar/baz')
+myvault.getSecret('kv/foo', 1)
+myvault.getSecret(mount: 'secret', path: 'foo')
+myvault.getSecret(mount: 'secret', path: 'foo/bar')
+myvault.getSecret(mount: 'secret', path: 'foo/bar/baz')
+myvault.getSecret(mount: 'secret', path: 'foo/bar/baz', dont_care: 'value')
+myvault.getSecret(mount: 'kv', path: 'foo')
+myvault.getSecret(mount: 'kv', path: 'foo/bar')
+myvault.getSecret(mount: 'kv', path: 'foo/bar/baz')
+myvault.getSecret(mount: 'kv', path: 'foo/bar/baz', dont_care: 'value')
+myvault.getSecret(mount: 'kv', path: 'foo', 1)
+myvault.findAllKeys('secret')
+myvault.findAllKeys('secret/')
+myvault.findAllKeys('secret/', 1)
+myvault.findAllKeys('secret/', 2)
+myvault.findAllKeys('secret/', 3)
+myvault.findAllKeys('kv')
+myvault.findAllKeys('kv/')
+myvault.findAllKeys('kv/', 1)
+myvault.findAllKeys('kv/', 2)
+myvault.findAllKeys('kv/', 3)
+myvault.listPath('secret')
+myvault.listPath('secret/')
+myvault.listPath('secret/foo')
+myvault.listPath('secret/foo/')
+myvault.listPath('secret/foo/bar')
+myvault.listPath('secret/foo/bar/')
+myvault.listPath('kv')
+myvault.listPath('kv/')
+myvault.listPath('kv/foo')
+myvault.listPath('kv/foo/')
+myvault.listPath('kv/foo/bar')
+myvault.listPath('kv/foo/bar/')
+
+println 'Success.'
+</tt></pre>
 
   */
 class VaultServiceTest extends GroovyTestCase {
