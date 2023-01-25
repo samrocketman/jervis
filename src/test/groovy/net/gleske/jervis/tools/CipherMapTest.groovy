8ab34c522f5d75ca261d4c72ad1cafc7ba1e3992
Sam Gleske <sam.mxracer@gmail.com>
2023-01-25 01:52:30 -0500

    Update tests and one failing

--- src/test/groovy/net/gleske/jervis/tools/CipherMapTest.groovy
+++ src/test/groovy/net/gleske/jervis/tools/CipherMapTest.groovy
@@ -94,12 +94,33 @@ class CipherMapTest extends GroovyTestCase {
             '''.stripIndent()
         assert ciphermap.plainMap == [:]
 
+        // signature validation fail due to corrupt data
         ciphermap.plainMap = [hello: 'friend']
         String ciphertext = ciphermap.toString()
         Map cipheryaml = YamlOperator.loadYamlFrom(ciphertext)
-        cipheryaml.age = 'corrupt the signature'
-        // fail signature
+        cipheryaml.age = 'corrupt the data'
+        ciphermap << YamlOperator.writeObjToYaml(cipheryaml)
+        assert ciphermap.plainMap == [:]
+
+        // signature validation fail due to corrupt signature
+        ciphermap.plainMap = [hello: 'friend']
+        ciphertext = ciphermap.toString()
+        cipheryaml = YamlOperator.loadYamlFrom(ciphertext)
+        cipheryaml.signature = 'corrupt the signature'
         ciphermap << YamlOperator.writeObjToYaml(cipheryaml)
         assert ciphermap.plainMap == [:]
     }
+    // TODO fix test
+    @Test public void test_CipherMap_rewrap_data_with_same_secrets() {
+        ciphermap.plainMap = [some: 'data']
+        Map yaml1 = YamlOperator.loadYamlFrom(ciphermap.toString())
+        ciphermap.plainMap = [change: 'data']
+        Map yaml2 = YamlOperator.loadYamlFrom(ciphermap.toString())
+        assert yaml1.age == yaml2.age
+        assert yaml1.cipher[0] == yaml2.cipher[0]
+        assert yaml1.cipher[1] == yaml2.cipher[1]
+        assert yaml1.data != yaml2.data
+        assert yaml1.signature != yaml2.signature
+    }
+    // TODO test rotating secrets due to age
 }
