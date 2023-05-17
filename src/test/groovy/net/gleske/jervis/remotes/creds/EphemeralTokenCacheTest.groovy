4d2e72deb83e3dda952902a21f8afda12229851f
Sam Gleske <sam.mxracer@gmail.com>
2023-05-17 18:13:33 -0400

    Initial EphermalTokenCache test

---
+++ src/test/groovy/net/gleske/jervis/remotes/creds/EphemeralTokenCacheTest.groovy
@@ -0,0 +1,40 @@
+/*
+   Copyright 2014-2023 Sam Gleske - https://github.com/samrocketman/jervis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+package net.gleske.jervis.remotes.creds
+//the EphemeralTokenCacheTest() class automatically sees the EphemeralTokenCache() class because they're in the same package
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class EphemeralTokenCacheTest extends GroovyTestCase {
+
+    /*
+    //set up before every test
+    @Before protected void setUp() {
+        super.setUp()
+    }
+    //tear down after every test
+    @After protected void tearDown() {
+        super.tearDown()
+    }
+    */
+    @Test public void test_EphemeralTokenCache_fail_instantiation() {
+        shouldFail(IllegalStateException) {
+            new EphemeralTokenCache()
+        }
+    }
+}
