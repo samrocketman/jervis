0a6e82c91d622f681455da8d2624c341baa2b15c
Sam Gleske <sam.mxracer@gmail.com>
2023-06-24 22:34:55 -0400

    Move getObjectValue tests

--- src/test/groovy/net/gleske/jervis/tools/YamlOperatorTest.groovy
+++ src/test/groovy/net/gleske/jervis/tools/YamlOperatorTest.groovy
@@ -92,4 +92,95 @@ class YamlOperatorTest extends GroovyAssert {
             YamlOperator.loadYamlFrom(yaml)
         }
     }
+    @Test public void test_YamlOperator_getObjectValue() {
+        Map example = [key1: [subkey1: 'string']]
+        assert 'string'.equals(generator.getObjectValue(example, 'key1.subkey1', 'default'))
+        assert 'default'.equals(generator.getObjectValue(example, 'key2.subkey1', 'default'))
+        assert 2.equals(generator.getObjectValue(example, 'key1.subkey1', 2))
+    }
+    @Test public void test_YamlOperator_getObjectValue_false_bug() {
+        Map example = [key1: [subkey1: 'false']]
+        assert false == generator.getObjectValue(example, 'key1.subkey1', false)
+        assert false == generator.getObjectValue(example, 'key1.subkey1', true)
+    }
+    @Test public void test_YamlOperator_getObjectValue_type_bug() {
+        Map example = [key1: [subkey1: 'string'],key2: ["a", "b"]]
+        assert 'default'.equals(generator.getObjectValue(example, 'key1', 'default'))
+        assert 'default'.equals(generator.getObjectValue(example, 'key2', 'default'))
+    }
+    @Test public void test_YamlOperator_getObjectValue_allow_conflicting_and_custom_lookup_paths() {
+        Map hexKeys = [ 'hello.io': 'world', 'hello': ['jervis.io{': 'friend']]
+
+        assert generator.getObjectValue(hexKeys, 'hello', 'hi') == 'hi'
+        assert generator.getObjectValue(hexKeys, 'hello\\.io', '') == 'world'
+        assert generator.getObjectValue(hexKeys, 'hello\\.io', []) == []
+        assert generator.getObjectValue(hexKeys, 'hello.jervis.io{', '') == ''
+        assert generator.getObjectValue(hexKeys, 'hello.jervis\\.io\\{', '') == 'friend'
+    }
+    @Test public void test_YamlOperator_getObjectValue_fallbacks_and_multiple_defaults() {
+        Map json = [
+            python: ['2.7': ['hello', 'world']],
+            type: [hello: 'world'],
+            friend: 'true',
+            bye: 'false'
+        ]
+
+        assert generator.getObjectValue(json, 'type // type.hello // python.2\\.7', ['', []]) == 'world'
+        assert generator.getObjectValue(json, 'type // type.hello // python.2\\.7', [[], '']) == 'world'
+        assert generator.getObjectValue(json, 'type.hello // python.2\\.7', [[], '']) == 'world'
+        assert generator.getObjectValue(json, 'type.hello // python.2\\.7', ['', []]) == 'world'
+        assert generator.getObjectValue(json, 'python.2\\.7 // type.hello', [[], '']) == ['hello', 'world']
+        assert generator.getObjectValue(json, 'python.2\\.7 // type.hello', ['', []]) == ['hello', 'world']
+
+        // no fallback but multiple defaults
+        assert generator.getObjectValue(json, 'python.2\\.7', ['', []]) == ['hello', 'world']
+        assert generator.getObjectValue(json, 'python.2\\.7', [[], '']) == ['hello', 'world']
+
+        // fallbacks with single default
+        assert generator.getObjectValue(json, 'type // type.hello // python.2\\.7', '') == 'world'
+        assert generator.getObjectValue(json, 'type // type.hello // python.2\\.7', []) == ['hello', 'world']
+        assert generator.getObjectValue(json, 'type.hello // python.2\\.7', true) == true
+        assert generator.getObjectValue(json, 'type.hello // python.2\\.7', false) == true
+        assert generator.getObjectValue(json, 'type.hello // python.2\\.7', [:]) == [:]
+        assert generator.getObjectValue(json, 'friend', true) == true
+        assert generator.getObjectValue(json, 'bye', true) == false
+        assert generator.getObjectValue(json, 'friend', false) == true
+        assert generator.getObjectValue(json, 'bye', false) == false
+        assert generator.getObjectValue(json, 'bye', [[], [:]]) == []
+        assert generator.getObjectValue(json, 'bye', [[:], []]) == [:]
+        assert generator.getObjectValue(json, 'friend // bye', [[:], []]) == [:]
+        assert generator.getObjectValue(json, 'bye // friend', [[:], []]) == [:]
+        assert generator.getObjectValue(json, 'friend // bye', [[], [:]]) == []
+        assert generator.getObjectValue(json, 'bye // friend', [[], [:]]) == []
+    }
+    @Test public void test_YamlOperator_getObjectValue_quoted_periods() {
+        Map json = [
+            python: ['2.7': ['hello', 'world']],
+            type: [hello: 'world']
+        ]
+
+        def benchmark = generator.getObjectValue(json, 'python.2\\.7', [])
+        assert benchmark == ['hello', 'world']
+        assert benchmark == generator.getObjectValue(json, 'python."2\\.7"', [])
+        assert benchmark == generator.getObjectValue(json, 'python."2.7"', [])
+    }
+    @Test public void test_YamlOperator_getObjectValue_groovydoc_examples() {
+        Map hexKeys = [
+            'hello.io': 'world',
+            hello: ['jervis.io{': 'friend'],
+            friend: [name: 'dog']
+        ]
+
+        assert generator.getObjectValue(hexKeys, 'friend.name', '') == 'dog'
+        assert generator.getObjectValue(hexKeys, 'friend.name', ['item']) == 'dog'
+        assert generator.getObjectValue(hexKeys, 'friend.name', [['item']]) == ['item']
+        assert generator.getObjectValue(hexKeys, 'hello\\.io', '') == 'world'
+        assert generator.getObjectValue(hexKeys, 'hello%{2e}io', '') == 'world'
+        assert generator.getObjectValue(hexKeys, 'hello.jervis\\.io\\{', '') == 'friend'
+        assert generator.getObjectValue(hexKeys, '"hello.io"', '') == 'world'
+        assert generator.getObjectValue(hexKeys, 'hello."jervis.io"{', '') == 'friend'
+        assert generator.getObjectValue(hexKeys, 'hello."jervis.io"{ // friend', [:]) == [name: 'dog']
+        assert generator.getObjectValue(hexKeys, 'hello', [ '', [] ]) == ''
+        assert generator.getObjectValue(hexKeys, 'hello // friend.name', [ '', [] ]) == 'dog'
+    }
 }
