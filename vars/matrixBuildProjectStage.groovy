ca1d0a6cd52b92572f62d08b485dbacbae849cc8
Sam Gleske <sam.mxracer@gmail.com>
2023-06-27 00:54:48 -0400

    withLocks limit 2

--- vars/matrixBuildProjectStage.groovy
+++ vars/matrixBuildProjectStage.groovy
@@ -29,7 +29,7 @@ def call(def global_scm, LifecycleGenerator generator, PipelineGenerator pipelin
         Map stashMap = pipeline_generator.getStashMap(matrix_axis)
         tasks[stageIdentifier] = {
             stage("Build axis ${stageIdentifier}") {
-                withLocks(obtain_lock: 'jervis', limit: 1) {
+                withLocks(obtain_lock: 'jervis', limit: 2) {
                 jervisBuildNode(pipeline_generator, label) {
                     Boolean failed_stage = false
                     withEnvSecretWrapper(pipeline_generator, axisEnvList) {
