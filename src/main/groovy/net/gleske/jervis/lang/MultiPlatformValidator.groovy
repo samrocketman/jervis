58ac3e1a0fa31e426420ac55dad24a2f3e26adfd
Sam Gleske <sam.mxracer@gmail.com>
2023-07-08 17:12:43 -0400

    Update package and license for MultiPlatform
    See https://github.com/samrocketman/jervis/issues/140

--- src/main/groovy/net/gleske/jervis/lang/MultiPlatformValidator.groovy
+++ src/main/groovy/net/gleske/jervis/lang/MultiPlatformValidator.groovy
@@ -1,11 +1,22 @@
-package net.gleske.jervis.beta
+/*
+   Copyright 2014-2023 Sam Gleske - https://github.com/samrocketman/jervis
 
-import net.gleske.jervis.lang.LifecycleGenerator
-import net.gleske.jervis.lang.LifecycleValidator
-import net.gleske.jervis.lang.PipelineGenerator
-import net.gleske.jervis.lang.PlatformValidator
-import net.gleske.jervis.lang.ToolchainValidator
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+package net.gleske.jervis.lang
+
 import net.gleske.jervis.tools.YamlOperator
+
 /**
 
 import net.gleske.jervis.beta.MultiPlatformValidator
