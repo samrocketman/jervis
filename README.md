4c4d7a703af83e59516ec69322fc6cda74162f74
Sam Gleske <sam.mxracer@gmail.com>
2025-12-09 22:55:34 -0500

    2025 license bump

--- README.md
+++ README.md
@@ -186,7 +186,7 @@ See [SonarQube README](sonarqube/README.md).
 
 # License
 
-    Copyright 2014-2024 Sam Gleske
+    Copyright 2014-2025 Sam Gleske
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- jobs/hack_class_loader.groovy
+++ jobs/hack_class_loader.groovy
@@ -1,5 +1,5 @@
 /*
-   Copyright 2014-2024 Sam Gleske - https://github.com/samrocketman/jervis
+   Copyright 2014-2025 Sam Gleske - https://github.com/samrocketman/jervis
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
